ec580ec117bc58803ae97f47c008c70f36ee8b28
AnibalAlves <a70859@alunos.uminho.pt>
2017-12-04 23:32:36 +0000

    wrking

--- sportyfield/app/src/main/java/eu/croussel/sportyfield/fieldInfo.java
+++ sportyfield/app/src/main/java/eu/croussel/sportyfield/fieldInfo.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class fieldInfo extends AppCompatActivity {
 
@@ -17,9 +19,26 @@ public class fieldInfo extends AppCompatActivity {
 
         db = new DataBaseHandler(getApplicationContext());
 
+        ImageView iv = (ImageView) findViewById(R.id.field_image);
+
         Intent intent = getIntent();
         int fieldId = getIntent().getIntExtra("fieldID",0); //get the field id from Maps class
+        TextView field_loc = (TextView) findViewById(R.id.field_location);
+        field f = db.getField(fieldId);
+        String theLocation = f.getLocation();
+        field_loc.setText(theLocation);
 
+        switch (fieldId) {
+            case 1:
+                iv.setImageResource(R.drawable.field);
+                break;
+            case 2:
+                iv.setImageResource(R.drawable.field);
+                break;
+            case 3:
+                iv.setImageResource(R.drawable.field);
+                break;
+        }
         //Creating a test user
         /*
         user u = new user("Test",25,"test@gmail.com",123456789,25,"Basketball");
