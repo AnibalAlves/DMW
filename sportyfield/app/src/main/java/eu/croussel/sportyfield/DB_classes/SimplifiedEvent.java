303bb2a480a7ac155d9edb01b7cf94cb7c5609f6
croussel <croussel@polytech-lille.net>
2018-02-04 15:48:51 +0100

    DB Handler create event & apply to event

---
+++ sportyfield/app/src/main/java/eu/croussel/sportyfield/DB_classes/SimplifiedEvent.java
@@ -0,0 +1,45 @@
+package eu.croussel.sportyfield.DB_classes;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by root on 04/02/18.
+ */
+
+public class SimplifiedEvent {
+        private int _eventId;
+        private int _fieldId;
+        private String _eventName;
+        private Date _eventDate;
+        private String _eventDescription;
+
+        public SimplifiedEvent(Event event){
+            _eventId = event.getEventId();
+            _fieldId = event.getFieldId();
+            _eventName = event.getEventName();
+            _eventDate = event.getEventDate();
+            _eventDescription = event.getEventDescription();
+        }
+
+        public SimplifiedEvent(int id, int fid, String name, Date d, String descri)
+        {
+            this._eventId=id;
+            this._fieldId=fid;
+            this._eventName=name;
+            this._eventDate=d;
+            this._eventDescription=descri;
+        }
+
+        public int getEventId(){return this._eventId;}
+        public void setEventId(int eventId){this._eventId = eventId;}
+        public int getFieldId(){return this._fieldId;}
+        public void setFieldId(int i){this._fieldId=i;}
+        public String getEventName() { return this._eventName;}
+        public void setEventName(String n){this._eventName=n;}
+        public Date getEventDate(){return this._eventDate;}
+        public void setEventDate(Date d){ this._eventDate=d;}
+        public String getEventDescription(){return this._eventDescription;}
+        public void setEventDescription(String d){this._eventDescription=d;}
+    }
+
